05c936f65ff490d5f9c7c8c1b234f14632534af9
Matheus de Oliveira <matheus82pessoal@protonmail.com>
2024-02-18 17:06:46 -0300

    implementing recipe list with items

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class RecipeListComponent {
 
+   recipes: Recipe[] = [
+    new Recipe("recipe one", "some description", "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.eatwell101.com%2Fwp-content%2Fuploads%2F2016%2F08%2Fhealthy-Chicken-Pasta-recipe.jpg&f=1&nofb=1&ipt=7d85a65c83bcea844a557b04d675670c84c7d0174b43f15b48f2da8edf99be72&ipo=images")
+   ];
 }
